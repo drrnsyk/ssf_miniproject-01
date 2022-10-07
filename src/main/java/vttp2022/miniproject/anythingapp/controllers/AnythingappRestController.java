b8d708ed0134831bd807eacefdd7d4db3007fb04
drrnsyk <darren.syk@gmail.com>
2022-10-07 10:25:38 +0800

    added change user, header and footer

--- src/main/java/vttp2022/miniproject/anythingapp/controllers/AnythingappRestController.java
+++ src/main/java/vttp2022/miniproject/anythingapp/controllers/AnythingappRestController.java
@@ -2,7 +2,6 @@ package vttp2022.miniproject.anythingapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
